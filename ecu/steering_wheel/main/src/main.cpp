71028234ef06c7dd33c5eeb2b466967006db6de7
d0nghwa <donghwa.kim17@gmail.com>
2024-11-16 14:59:21 -0800

    feat: integrate arduino software into firmware repository

---
+++ ecu/steering_wheel/main/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "serial.h"
+
+int myFunction(int, int);
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("BEGIN SERIAL COMMUNICATION");
+}
+
+void loop() {
+  SerialMessage message = createSerialMessage(0xAF, 0xFF);
+
+  sendSerialMessage(message);
+
+  delay(2000);
+}
