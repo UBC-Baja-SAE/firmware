b2511cee5c4749c69bcad2b9b31c9261cd43fc6c
Donghwa Kim <donghwak@student.ubc.ca>
2025-02-22 11:09:50 -0800

    fix: scale Canvas functions to match dp sizing in Composables

--- dashboard-compose/src/main/kotlin/view/gauge/linear/LinearGauge.kt
+++ dashboard-compose/src/main/kotlin/view/gauge/linear/LinearGauge.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -17,6 +18,7 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.baja.dashboard.view.resources.BAJA_PURPLE
 
 /**
@@ -54,30 +56,39 @@ private const val OFFSET = BORDER_WIDTH / 2
 /**
  * The length of the center tick and the outer ticks.
  */
-private const val TICK_LENGTH_SMALL = BAR_WIDTH * 0.6f + BORDER_WIDTH
-private const val TICK_LENGTH_LARGE = BAR_WIDTH + BORDER_WIDTH
+private const val TICK_LENGTH_SMALL = BAR_WIDTH * 0.8f
+private const val TICK_LENGTH_LARGE = BAR_WIDTH
 
 /**
  * The size of the icons next to the center of the gauge.
  */
-private const val IMAGE_SIZE = 40
+private const val IMAGE_SIZE = 30
+
+/**
+ * The font size of the symbols next to the tick marks.
+ */
+private const val SYMBOL_FONT_SIZE = 20
 
 /**
  * A gauge that fills linearly vertically. The gauge fills from bottom to top,
  * and it is fully filled when `currentValue` is equal to `maxValue`.
  * @param modifier  the modifier for the `LinearGauge`'s bounding box. Use this
- *                  parameter to modify the position of the gauge.
+ *  parameter to modify the position of the gauge.
  * @param currentValue  the current value to display on the gauge.
  * @param maxValue  the maximum value that can be displayed on the gauge.
  * @param imagePath the filepath for the icon to display, relative to the
- *                  `src/main/resources` directory.
+ *  src/main/resources` directory.
+ * @param symbols   the symbols that indicate the meaning of the gauge when it
+ *  is full or empty. The elements will be drawn with the `first` and `second`
+ *  at the bottom and top of the gauge respectively.
  */
 @Composable
 fun LinearGauge(
     modifier: Modifier,
     currentValue: Float,
     maxValue: Float,
-    imagePath: String
+    imagePath: String,
+    symbols: Pair<Char, Char>
 ) {
     val percentage = if (currentValue >= 0f) currentValue / maxValue else 0f
     val targetHeight by animateFloatAsState(
@@ -94,19 +105,27 @@ fun LinearGauge(
             .width(BAR_WIDTH.dp),
         contentAlignment = Alignment.Center
     ) {
-        Canvas(modifier = Modifier.matchParentSize()) {
-            val left = -OFFSET
-            val right = TICK_LENGTH_LARGE
-            val top = left
-            val bottom = OFFSET + BAR_HEIGHT
-            val center = BAR_HEIGHT / 2
+        val left = -OFFSET
+        val right = TICK_LENGTH_LARGE
+        val top = left
+        val bottom = BAR_HEIGHT + OFFSET
+        val center = BAR_HEIGHT / 2
+
+        Canvas(
+            modifier = Modifier
+                .height(BAR_HEIGHT.dp)
+                .width(BAR_WIDTH.dp)
+        ) {
             drawLine(left, top, left, bottom)
             drawLine(left, top, right, top)
             drawLine(left, bottom, right, bottom)
             drawLine(left, center, TICK_LENGTH_SMALL, center)
+
             fillGauge(targetHeight)
         }
-        DrawIcon(imagePath)
+        Icon(imagePath)
+        Symbol(symbols.first, bottom)
+        Symbol(symbols.second, top)
     }
 }
 
@@ -116,16 +135,33 @@ fun LinearGauge(
  *                  `src/main/resources` directory.
  */
 @Composable
-fun DrawIcon(imagePath: String) {
+fun Icon(imagePath: String) {
     Image(
         painter = painterResource(imagePath),
         contentDescription = "",
         modifier = Modifier
             .size(IMAGE_SIZE.dp)
             .offset(
-                (TICK_LENGTH_LARGE / 2 + IMAGE_SIZE / 2).dp,
-                (BORDER_WIDTH / 2).dp
+                x = (TICK_LENGTH_LARGE / 2 + IMAGE_SIZE / 2).dp
             )
+    )
+}
+
+/**
+ * Draws the [symbol] at the end of the tick mark at [height].
+ * @param symbol    the symbol to be displayed.
+ * @param height    the height to draw the symbol starting at.
+ */
+@Composable
+fun Symbol(symbol: Char, height: Float) {
+    Text(
+        text = "$symbol",
+        fontSize = SYMBOL_FONT_SIZE.sp,
+        color = Color.White,
+        modifier = Modifier.offset(
+            x = (TICK_LENGTH_LARGE / 2 + SYMBOL_FONT_SIZE / 2).dp,
+            y = (height - BAR_HEIGHT / 2).dp
+        )
     )
 }
 
@@ -138,11 +174,12 @@ fun DrawScope.fillGauge(targetHeight: Float) {
     val gradientBrush = Brush.linearGradient(
         colorStops = GRADIENT
     )
+    val x = (BAR_WIDTH / 2).dp.toPx()
     drawLine(
         brush = gradientBrush,
-        start = Offset(BAR_WIDTH / 2, BAR_HEIGHT),
-        end = Offset(BAR_WIDTH / 2, BAR_HEIGHT - targetHeight),
-        strokeWidth = BAR_WIDTH
+        start = Offset(x, BAR_HEIGHT.dp.toPx()),
+        end = Offset(x, (BAR_HEIGHT - targetHeight).dp.toPx()),
+        strokeWidth = BAR_WIDTH.dp.toPx()
     )
 }
 
@@ -153,8 +190,8 @@ fun DrawScope.drawLine(x1: Float, y1: Float, x2: Float, y2: Float) {
     drawLine(
         color = Color.White,
         strokeWidth = BORDER_WIDTH,
-        start = Offset(x1, y1),
-        end = Offset(x2, y2),
+        start = Offset(x1.dp.toPx(), y1.dp.toPx()),
+        end = Offset(x2.dp.toPx(), y2.dp.toPx()),
         cap = StrokeCap.Square
     )
 }
