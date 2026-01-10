d9b11552f4e4bf22641622ba0fc6b149fd3873b5
Geoffrey Bian <geoffreybian100@gmail.com>
2026-01-10 14:40:50 -0800

    fix errors in rear pcb compilation

--- projects/rear_pcb/Core/Inc/control.h
+++ projects/rear_pcb/Core/Inc/control.h
@@ -1,6 +1,10 @@
 #ifndef __CONTROL_H
 #define __CONTROL_H
 
+#include <stdint.h>
+#include "main.h"
+//#include "stm32h7xx_hal.h"
+
 void SendRPMOnCan(uint32_t can_id);
 void SendSpeedOnCan(uint32_t can_id);
 void SendFuelOnCan(uint32_t can_id);
--- projects/rear_pcb/Core/Src/control.c
+++ projects/rear_pcb/Core/Src/control.c
@@ -1,22 +1,25 @@
 #include "control.h"
 
-static void CAN_Transmit(uint32_t id, uint8_t *data, uint32_t len) {
-    FDCAN_TxHeaderTypeDef tx_header = {
-        .Identifier         = id,
-        .IdType             = FDCAN_STANDARD_ID,
-        .TxFrameType        = FDCAN_DATA_FRAME,
-        .DataLength         = len,
-        .ErrorStateIndicator= FDCAN_ESI_ACTIVE,
-        .BitRateSwitch      = FDCAN_BRS_OFF,
-        .FDFormat           = FDCAN_CLASSIC_CAN,
-        .TxEventFifoControl = FDCAN_NO_TX_EVENTS,
-        .MessageMarker      = 0
-    };
+extern FDCAN_HandleTypeDef hfdcan1;
+//extern ADC_HandleTypeDef hadc1;
 
-    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &tx_header, data) != HAL_OK) {
-        Error_Handler();
-    }
-}
+//static void CAN_Transmit(uint32_t id, uint8_t *data, uint32_t len) {
+//    FDCAN_TxHeaderTypeDef tx_header = {
+//        .Identifier         = id,
+//        .IdType             = FDCAN_STANDARD_ID,
+//        .TxFrameType        = FDCAN_DATA_FRAME,
+//        .DataLength         = len,
+//        .ErrorStateIndicator= FDCAN_ESI_ACTIVE,
+//        .BitRateSwitch      = FDCAN_BRS_OFF,
+//        .FDFormat           = FDCAN_CLASSIC_CAN,
+//        .TxEventFifoControl = FDCAN_NO_TX_EVENTS,
+//        .MessageMarker      = 0
+//    };
+//
+//    if (HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &tx_header, data) != HAL_OK) {
+//        Error_Handler();
+//    }
+//}
 
 void SendRPMOnCan(uint32_t can_id)
 {
