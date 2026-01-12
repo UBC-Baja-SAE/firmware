8c34db0fe307528824f917685b75f4249db1e673
Geoffrey Bian <geoffreybian100@gmail.com>
2026-01-11 22:12:14 -0800

    clean can dev esus -> REAR PCB

--- projects/can_dev_esus/Core/Inc/control.h
+++ projects/can_dev_esus/Core/Inc/control.h
@@ -3,18 +3,8 @@
 
 #include "main.h"
 
-#define X_EXTEND_CM     72.39f
-#define X_COMP_CM       47.625f
-#define V_EXTEND        0.0f
-#define V_COMP          5.0f
-
 extern volatile uint32_t measured_frequency;
 
-float VoltageToPosition(float voltage);
-void SendPotOnCan(uint32_t can_id);
-void SendAccelOnCan(uint32_t can_id);
-void SendGyroOnCan(uint32_t can_id);
-void SendStrainOnCan(uint32_t can_id);
 void SendFrequencyOnCan(uint32_t can_id); //new!
 
 #endif // __CONTROL_H
--- projects/can_dev_esus/Core/Src/main.c
+++ projects/can_dev_esus/Core/Src/main.c
@@ -155,42 +155,6 @@ int main(void)
 
     SendFrequencyOnCan(0x600);
 
-    /*
-    // FL
-    SendPotOnCan(CAN_ID_ESUS_FL_SUSPENSION);
-    SendAccelOnCan(CAN_ID_ESUS_FL_IMU_ACCEL);
-    SendGyroOnCan(CAN_ID_ESUS_FL_IMU_GYRO);
-    SendStrainOnCan(CAN_ID_ESUS_FL_STRAIN_L);
-    SendStrainOnCan(CAN_ID_ESUS_FL_STRAIN_R);
-    */
-
-    /*
-    // FR
-    SendPotOnCan(CAN_ID_ESUS_FR_SUSPENSION);
-    SendAccelOnCan(CAN_ID_ESUS_FR_IMU_ACCEL);
-    SendGyroOnCan(CAN_ID_ESUS_FR_IMU_GYRO);
-    SendStrainOnCan(CAN_ID_ESUS_FR_STRAIN_L);
-    SendStrainOnCan(CAN_ID_ESUS_FR_STRAIN_R);
-    */
-
-    /*
-    // RR
-    SendPotOnCan(CAN_ID_ESUS_RR_SUSPENSION);
-    SendAccelOnCan(CAN_ID_ESUS_RR_IMU_ACCEL);
-    SendGyroOnCan(CAN_ID_ESUS_RR_IMU_GYRO);
-    SendStrainOnCan(CAN_ID_ESUS_RR_STRAIN_L);
-    SendStrainOnCan(CAN_ID_ESUS_RR_STRAIN_R);
-    */
-
-    /*
-    // RL
-    SendPotOnCan(CAN_ID_ESUS_Rl_SUSPENSION);
-    SendAccelOnCan(CAN_ID_ESUS_RL_IMU_ACCEL);
-    SendGyroOnCan(CAN_ID_ESUS_RL_IMU_GYRO);
-    SendStrainOnCan(CAN_ID_ESUS_RL_STRAIN_L);
-    SendStrainOnCan(CAN_ID_ESUS_RL_STRAIN_R);
-    */
-
     // Send message every 100 ms
     HAL_Delay(100);
   }
