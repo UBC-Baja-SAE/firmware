4521434207fd15c0dbec817addf9d5c5394ef2ad
d0nghwa <donghwa.kim17@gmail.com>
2026-01-15 05:42:09 +0000

    add all signals from esus and rear to pi dashboard

--- projects/dashboard-compose/src/main/cpp/can_bridge.cpp
+++ projects/dashboard-compose/src/main/cpp/can_bridge.cpp
@@ -19,6 +19,7 @@ double getData(int id)
     return result;
 }
 
+// --- Standard Dashboard Signals ---
 getSpeed
 {
     return (jdouble) getData(speedometer_id);
@@ -37,6 +38,121 @@ getRPM
 getFuel
 {
     return (jdouble) getData(fuel_sensor_id);
+}
+
+// --- Front Left ECU Signals ---
+getFLAccel
+{
+    return (jdouble) getData(fl_imu_accel_id);
+}
+
+getFLGyro
+{
+    return (jdouble) getData(fl_imu_gyro_id);
+}
+
+getFLSuspension
+{
+    return (jdouble) getData(fl_suspension);
+}
+
+getFLStrainL
+{
+    return (jdouble) getData(fl_strain_l_id);
+}
+
+getFLStrainR
+{
+    return (jdouble) getData(fl_strain_r_id);
+}
+
+// --- Front Right ECU Signals ---
+getFRAccel
+{
+    return (jdouble) getData(fr_imu_accel_id);
+}
+
+getFRGyro
+{
+    return (jdouble) getData(fr_imu_gyro_id);
+}
+
+getFRSuspension
+{
+    return (jdouble) getData(fr_suspension);
+}
+
+getFRStrainL
+{
+    return (jdouble) getData(fr_strain_l_id);
+}
+
+getFRStrainR
+{
+    return (jdouble) getData(fr_strain_r_id);
+}
+
+// --- Rear Left ECU Signals ---
+getRLAccel
+{
+    return (jdouble) getData(rl_imu_accel_id);
+}
+
+getRLGyro
+{
+    return (jdouble) getData(rl_imu_gyro_id);
+}
+
+getRLSuspension
+{
+    return (jdouble) getData(rl_suspension);
+}
+
+getRLStrainL
+{
+    return (jdouble) getData(rl_strain_l_id);
+}
+
+getRLStrainR
+{
+    return (jdouble) getData(rl_strain_r_id);
+}
+
+// --- Rear Right ECU Signals ---
+getRRAccel
+{
+    return (jdouble) getData(rr_imu_accel_id);
+}
+
+getRRGyro
+{
+    return (jdouble) getData(rr_imu_gyro_id);
+}
+
+getRRSuspension
+{
+    return (jdouble) getData(rr_suspension);
+}
+
+getRRStrainL
+{
+    return (jdouble) getData(rr_strain_l_id);
+}
+
+getRRStrainR
+{
+    return (jdouble) getData(rr_strain_r_id);
+}
+
+// --- Pi IMU ---
+getPiAccel
+{
+    return (jdouble) getData(pi_imu_accel_id);    
+}
+
+getPiGyro
+{
+    return (jdouble) getData(pi_imu_gyro_id);
 }
 
 startProcessor
--- projects/dashboard-compose/src/main/kotlin/viewmodel/DataViewModel.kt
+++ projects/dashboard-compose/src/main/kotlin/viewmodel/DataViewModel.kt
@@ -10,14 +10,42 @@ import org.baja.dashboard.model.DataRepository
 
 object DataViewModel {
     private val repository = DataRepository()
-
     private val speed = MutableStateFlow(0.0)
-
     private val temperature = MutableStateFlow(0.0)
-
     private val rpm = MutableStateFlow(0.0)
-
     private val fuel = MutableStateFlow(0.0)
+
+    // --- Front Left ECU Flows ---
+    private val flAccel = MutableStateFlow(0.0)
+    private val flGyro = MutableStateFlow(0.0)
+    private val flSuspension = MutableStateFlow(0.0)
+    private val flStrainL = MutableStateFlow(0.0)
+    private val flStrainR = MutableStateFlow(0.0)
+
+    // --- Front Right ECU Flows ---
+    private val frAccel = MutableStateFlow(0.0)
+    private val frGyro = MutableStateFlow(0.0)
+    private val frSuspension = MutableStateFlow(0.0)
+    private val frStrainL = MutableStateFlow(0.0)
+    private val frStrainR = MutableStateFlow(0.0)
+
+    // --- Rear Left ECU Flows ---
+    private val rlAccel = MutableStateFlow(0.0)
+    private val rlGyro = MutableStateFlow(0.0)
+    private val rlSuspension = MutableStateFlow(0.0)
+    private val rlStrainL = MutableStateFlow(0.0)
+    private val rlStrainR = MutableStateFlow(0.0)
+
+    // --- Rear Right ECU Flows ---
+    private val rrAccel = MutableStateFlow(0.0)
+    private val rrGyro = MutableStateFlow(0.0)
+    private val rrSuspension = MutableStateFlow(0.0)
+    private val rrStrainL = MutableStateFlow(0.0)
+    private val rrStrainR = MutableStateFlow(0.0)
+
+    // --- Pi IMU Flows ---
+    private val piAccel = MutableStateFlow(0.0)
+    private val piGyro = MutableStateFlow(0.0)
 
     init {
         CoroutineScope(Dispatchers.IO).launch {
@@ -30,12 +58,36 @@ object DataViewModel {
     }
 
     fun getSpeed() = speed.asStateFlow()
-
     fun getTemperature() = temperature.asStateFlow()
-
     fun getRPM() = rpm.asStateFlow()
-
     fun getFuel() = fuel.asStateFlow()
+
+    fun getFLAccel() = flAccel.asStateFlow()
+    fun getFLGyro() = flGyro.asStateFlow()
+    fun getFLSuspension() = flSuspension.asStateFlow()
+    fun getFLStrainL() = flStrainL.asStateFlow()
+    fun getFLStrainR() = flStrainR.asStateFlow()
+
+    fun getFRAccel() = frAccel.asStateFlow()
+    fun getFRGyro() = frGyro.asStateFlow()
+    fun getFRSuspension() = frSuspension.asStateFlow()
+    fun getFRStrainL() = frStrainL.asStateFlow()
+    fun getFRStrainR() = frStrainR.asStateFlow()
+
+    fun getRLAccel() = rlAccel.asStateFlow()
+    fun getRLGyro() = rlGyro.asStateFlow()
+    fun getRLSuspension() = rlSuspension.asStateFlow()
+    fun getRLStrainL() = rlStrainL.asStateFlow()
+    fun getRLStrainR() = rlStrainR.asStateFlow()
+
+    fun getRRAccel() = rrAccel.asStateFlow()
+    fun getRRGyro() = rrGyro.asStateFlow()
+    fun getRRSuspension() = rrSuspension.asStateFlow()
+    fun getRRStrainL() = rrStrainL.asStateFlow()
+    fun getRRStrainR() = rrStrainR.asStateFlow()
+
+    fun getPiAccel() = piAccel.asStateFlow()
+    fun getPiGyro() = piGyro.asStateFlow()
 
     private suspend fun startDataCollection() {
         while (true) {
@@ -52,5 +104,37 @@ object DataViewModel {
         temperature.value = repository.getTemperature()
         rpm.value = repository.getRPM()
         fuel.value = repository.getFuel()
+
+        // FL
+        flAccel.value = repository.getFLAccel()
+        flGyro.value = repository.getFLGyro()
+        flSuspension.value = repository.getFLSuspension()
+        flStrainL.value = repository.getFLStrainL()
+        flStrainR.value = repository.getFLStrainR()
+
+        // FR
+        frAccel.value = repository.getFRAccel()
+        frGyro.value = repository.getFRGyro()
+        frSuspension.value = repository.getFRSuspension()
+        frStrainL.value = repository.getFRStrainL()
+        frStrainR.value = repository.getFRStrainR()
+
+        // RL
+        rlAccel.value = repository.getRLAccel()
+        rlGyro.value = repository.getRLGyro()
+        rlSuspension.value = repository.getRLSuspension()
+        rlStrainL.value = repository.getRLStrainL()
+        rlStrainR.value = repository.getRLStrainR()
+
+        // RR
+        rrAccel.value = repository.getRRAccel()
+        rrGyro.value = repository.getRRGyro()
+        rrSuspension.value = repository.getRRSuspension()
+        rrStrainL.value = repository.getRRStrainL()
+        rrStrainR.value = repository.getRRStrainR()
+
+        // Pi
+        piAccel.value = repository.getPiAccel()
+        piGyro.value = repository.getPiGyro()
     }
 }
