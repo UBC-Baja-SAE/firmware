db6ef6a997372fe8fd9832b3344f64242b5acbce
benjfriesen <bfrzn@student.ubc.ca>
2026-06-09 01:26:52 -0700

    new docker container

--- projects/ros2can/src/stm32_can_bridge/launch/canopen.launch.py
+++ projects/ros2can/src/stm32_can_bridge/launch/canopen.launch.py
@@ -6,11 +6,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
     pkg_dir = get_package_share_directory('stm32_can_bridge')
 
-    # Define ALL three file paths
-    # Use explicit absolute paths inside the container
     bus_config_yml = '/ros2_ws/install/stm32_can_bridge/share/stm32_can_bridge/config/stm32_bus/bus.yml'
     master_config_dcf = '/ros2_ws/install/stm32_can_bridge/share/stm32_can_bridge/config/stm32_bus/master.dcf'
-    master_config_bin = '/ros2_ws/install/stm32_can_bridge/share/stm32_can_bridge/config/stm32_bus/master.bin'
 
     device_container_node = Node(
         package='canopen_core',
@@ -20,7 +17,6 @@ def generate_launch_description():
         parameters=[
             {'bus_config': bus_config_yml},
             {'master_config': master_config_dcf},
-            {'master_bin': master_config_bin}, # <-- Added the missing comma here
             {'can_interface_name': 'vcan0'}
         ]
     )
