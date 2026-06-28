4e9b3f78c3db8fad16bfffa1e75433c46d10e150
benjfriesen <bfrzn@student.ubc.ca>
2026-06-27 17:15:44 -0700

    minor changes

--- projects/dash/src/core/microphone.cpp
+++ projects/dash/src/core/microphone.cpp
@@ -2,10 +2,22 @@
 #include <QMediaDevices>
 #include <QAudioDevice>
 #include <QAudioFormat>
-#include <QTimer> // <-- ADD THIS
+#include <QTimer>
 #include <QDebug>
 
-// ... (Your constructor stays the same) ...
+Microphone::Microphone(QObject* parent) : QObject(parent) {
+    QAudioFormat format;
+    format.setSampleRate(m_sampleRate);
+    format.setChannelCount(m_channels);
+    format.setSampleFormat(QAudioFormat::Int16); // Sets standard PCM 16-bit format
+
+    QAudioDevice info = QMediaDevices::defaultAudioInput();
+    if (!info.isFormatSupported(format)) {
+        qWarning() << "[Microphone] Default format not supported. Audio may sound distorted.";
+    }
+
+    m_audioSource = new QAudioSource(info, format, this);
+}
 
 void Microphone::start() {
     // 1. Check if the Linux kernel has actually enumerated any audio devices yet
@@ -24,5 +36,14 @@ void Microphone::start() {
         qInfo() << "[Microphone] Started capturing live audio";
     } else {
         qWarning() << "[Microphone] Failed to open audio hardware";
+    }
+}
+
+void Microphone::processAudio() {
+    if (!m_ioDevice) return;
+
+    QByteArray data = m_ioDevice->readAll();
+    if (data.size() > 0) {
+        emit audioReady("/camera/audio", data, m_sampleRate, m_channels);
     }
 }
