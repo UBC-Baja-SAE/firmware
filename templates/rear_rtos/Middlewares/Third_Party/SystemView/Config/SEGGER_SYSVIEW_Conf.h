a64016770a84a5d1d9d377ffe0885c3ce5dd3896
bfrzn <bfrzn@student.ubc.ca>
2026-08-26 13:26:16 -0700

    RTOS with SystemView init template

---
+++ templates/rear_rtos/Middlewares/Third_Party/SystemView/Config/SEGGER_SYSVIEW_Conf.h
@@ -0,0 +1,80 @@
+/*********************************************************************
+*                   (c) SEGGER Microcontroller GmbH                  *
+*                        The Embedded Experts                        *
+*                           www.segger.com                           *
+**********************************************************************
+*                                                                    *
+*         SEGGER SystemView  * Real-time application analysis        *
+*              https://github.com/SEGGERMicro/SystemView             *
+*                                                                    *
+**********************************************************************
+
+---------------------------END-OF-HEADER------------------------------
+
+Purpose : SEGGER SystemView configuration file.
+          Set defines which deviate from the defaults (see SEGGER_SYSVIEW_ConfDefaults.h) here.          
+
+Additional information:
+  Required defines which must be set are:
+    SEGGER_SYSVIEW_GET_TIMESTAMP
+    SEGGER_SYSVIEW_GET_INTERRUPT_ID
+  For known compilers and cores, these might be set to good defaults
+  in SEGGER_SYSVIEW_ConfDefaults.h.
+  
+  SystemView needs a (nestable) locking mechanism.
+  If not defined, the RTT locking mechanism is used,
+  which then needs to be properly configured.
+*/
+
+#ifndef SEGGER_SYSVIEW_CONF_H
+#define SEGGER_SYSVIEW_CONF_H
+
+/*********************************************************************
+*
+*       Defines, configurable
+*
+**********************************************************************
+*/
+
+/*********************************************************************
+*
+*       Define: SEGGER_SYSVIEW_SECTION
+*
+*  Description
+*    Section to place the SystemView RTT Buffer into.
+*  Default
+*    undefined: Do not place into a specific section.
+*  Notes
+*    If SEGGER_RTT_SECTION is defined, the default changes to use
+*    this section for the SystemView RTT Buffer, too.
+*/
+#if !(defined SEGGER_SYSVIEW_SECTION) && (defined SEGGER_RTT_BUFFER_SECTION)
+  #define SEGGER_SYSVIEW_SECTION                  SEGGER_RTT_BUFFER_SECTION
+#endif
+
+
+/*********************************************************************
+* TODO: Add your defines here.                                       *
+**********************************************************************
+*/
+
+#include <stdint.h>
+
+// Tell SystemView we are using a Cortex-M core and an OS (FreeRTOS)
+#define SEGGER_SYSVIEW_CORE               SEGGER_SYSVIEW_CORE_CM3
+#define SEGGER_SYSVIEW_OS                 1
+
+// Read directly from the Cortex-M7 DWT Cycle Counter hardware register
+#define SEGGER_SYSVIEW_GET_TIMESTAMP()    (*(volatile unsigned int *)(0xE0001004))
+
+// Pull the global clock speed variable
+extern uint32_t SystemCoreClock;
+#define SEGGER_SYSVIEW_TIMESTAMP_FREQ     SystemCoreClock
+
+// Read directly from the Cortex-M7 ICSR register (IPSR bits)
+#define SEGGER_SYSVIEW_GET_INTERRUPT_ID() ((*(volatile unsigned int *)(0xE000ED04)) & 0x1FF)
+
+
+#endif  // SEGGER_SYSVIEW_CONF_H
+
+/*************************** End of file ****************************/
